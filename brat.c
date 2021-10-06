3c10beba5f03f26b186120a0f02efac6030fdc83
AMIdev <73357909+OnexIndieStudios@users.noreply.github.com>
2021-10-06 08:16:00 +0200

    Add files via upload

---
+++ brat.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+
+int main(){
+char ime[30];
+char ime2[30];
+
+printf("Vpisi svoje ime: ");
+scanf("%s", &ime); /* Vpisi ime */
+
+int lenght = strlen(ime) - 1;
+int a = 0;
+int b = lenght;
+
+while(1 < 2){
+ a++;
+ b--
+}
+
+printf("\n%d\n", lenght);
+return 0;
+}
